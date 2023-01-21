2b60fcba9d71b10f3fc7b97a37fcd115a1c992ba
Human <54731850+Ramiels@users.noreply.github.com>
2023-01-20 22:55:04 -0500

    noita mod files

---
+++ init.lua
@@ -0,0 +1,24 @@
+
+--[[ CALLBACK CODE GOES HERE. NO CODE ALLOWED, ONLY REFERENCE PREVIOUS FUNCTIONS.
+  ██████████                                                                                                                    
+██          ██      ████      ██            ██            ██████████        ████        ████████    ██        ██    ████████    
+██                ██    ██    ██            ██            ██        ██    ██    ██    ██        ██  ██      ██    ██            
+██              ██        ██  ██            ██            ██████████    ██        ██  ██            ████████        ████████    
+██              ████████████  ██            ██            ██        ██  ████████████  ██            ██    ██                ██  
+██          ██  ██        ██  ██            ██            ██        ██  ██        ██  ██        ██  ██      ██    ██        ██  
+  ██████████    ██        ██  ████████████  ████████████  ██████████    ██        ██    ████████    ██        ██    ████████    
+]]
+
+function OnModPreInit() end
+function OnModInit() end
+function OnModPostInit() end
+function OnPlayerSpawned( player_entity ) end
+function OnPlayerDied( player_entity ) end
+function OnWorldInitialized() end
+function OnWorldPreUpdate() end
+function OnWorldPostUpdate() end
+function OnBiomeConfigLoaded() end
+function OnMagicNumbersAndWorldSeedInitialized() end
+function OnPausedChanged( is_paused, is_inventory_pause ) end
+function OnModSettingsChanged() end
+function OnPausePreUpdate() end
